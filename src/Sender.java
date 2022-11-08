0de8731e3e7b3df4aaaa60274bc24311ecdb4c8e
IvanRdaw <ivanr.daw@gmail.com>
2022-11-08 18:13:56 +0100

    Calcular bits de redundacia

--- src/Sender.java
+++ src/Sender.java
@@ -1,4 +1,26 @@
 
 public class Sender {
 
+	
+	public static void main(String args[]) {
+		
+		int length = 27;
+
+		int bitsParidad = 0;
+		int formula =   bitsParidad + length + 1;
+		double potencia = Math.pow(2, bitsParidad);
+		
+		while ( potencia < formula) {
+			
+			
+			bitsParidad++;
+			
+			formula =   bitsParidad + length + 1;
+			potencia = Math.pow(2, bitsParidad);
+			
+		}
+		
+	
+	
+	}
 }
