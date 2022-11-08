a35d18379150f82987bf033ab44af65fab5a24cd
IvanRdaw <ivanr.daw@gmail.com>
2022-11-08 19:32:10 +0100

    Añado metodo que comprueba si un numero es potencia de 2

--- src/Sender.java
+++ src/Sender.java
@@ -4,10 +4,10 @@ public class Sender {
 	
 	public static void main(String args[]) {
 		
-		int length = 27;
+		int[] mensaje = {0,1,1,0,1,1,0,1};
 
 		int bitsParidad = 0;
-		int formula =   bitsParidad + length + 1;
+		int formula =  bitsParidad + mensaje.length + 1;
 		double potencia = Math.pow(2, bitsParidad);
 		
 		while ( potencia < formula) {
@@ -15,12 +15,45 @@ public class Sender {
 			
 			bitsParidad++;
 			
-			formula =   bitsParidad + length + 1;
+			formula =   bitsParidad + mensaje.length + 1;
 			potencia = Math.pow(2, bitsParidad);
 			
 		}
 		
+		
 	
-	
+		
+		
+		
 	}
+	
+	private static boolean esPotenciaDeDos(int posicion) {
+		
+		boolean esPotencia = true;
+		
+		if(posicion == 0)
+			esPotencia = false;
+		else if(posicion == 1)
+			esPotencia = true;
+		
+		else {
+			
+			while(posicion >= 2) {
+			
+				if(posicion %2 != 0)
+					esPotencia = false;
+				
+				posicion /= 2;
+				
+			}
+			
+		}			
+	  
+		return esPotencia;
+		
+	}
+	
+	
+	
+	
 }
